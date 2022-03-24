05ccb557a448445be41010526a422980885183a7
naomy <92695534+nnoguez@users.noreply.github.com>
2022-03-24 18:50:04 -0400

    March 24, 2022 6:50 PM

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ function MenuChoices(props) {
   }
 }
 
-export function Greet(props) {
+export function Greet() {
   const [greet, setGreet] = useState(false);
   return(
     <div className="Greet">
@@ -37,7 +37,7 @@ export function Greet(props) {
           </>
           :
           <>
-          <MenuChoices isChoice={choice} data1={appData} data2={entrData} data3={desData} data4={bevData}/>
+          <App/>
           </>
         )
       }
