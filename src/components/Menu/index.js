aee8fb97f57617252c5ccdf53a675d12745b13b1
naomy <92695534+nnoguez@users.noreply.github.com>
2022-04-19 00:49:03 -0400

    April 19, 2022 12:49 AM

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -18,5 +18,32 @@ export default class Menu extends React.Component {
         )
     }
 }
+import * as React from "react";
+import { Card } from "react-native-elements";
+import { View, Image } from "react-native";
 
+export default () => {
+  return (
+    <Card containerStyle={{}} wrapperStyle={{}}>
+      <Card.Title>CARD WITH DIVIDER</Card.Title>
+      <Card.Divider />
+      <View
+        style={{
+          position: "relative",
+          alignItems: "center"
+        }}
+      >
+        <Image
+          style={{ width: "100%", height: 100 }}
+          resizeMode="contain"
+          source={{
+            uri:
+              "https://avatars0.githubusercontent.com/u/32242596?s=460&u=1ea285743fc4b083f95d6ee0be2e7bb8dcfc676e&v=4"
+          }}
+        />
+        <Text>Pranshu Chittora</Text>
+      </View>
+    </Card>
+  );
+}
 
